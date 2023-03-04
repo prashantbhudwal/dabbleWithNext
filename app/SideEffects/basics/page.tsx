3eba6c838a28d357ebd9b7f624f1d01ac3b3409b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-04 09:48:48 +0530

    LO: useEffect is used to manage SideEffects

--- app/SideEffects/basics/page.tsx
+++ app/SideEffects/basics/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Basics() {
-  return <>Basics of Side Effects</>;
+  console.log("render");
+  const [count, setCount] = useState(0);
+  const [test, setTest] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count} Test:${test}`;
+  });
+
+  return (
+    <div>
+      <p>Count Value: {count}</p>
+      <p>Test Value:{test}</p>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+      <button onClick={() => setTest(test + 1)}>Test</button>
+    </div>
+  );
 }
