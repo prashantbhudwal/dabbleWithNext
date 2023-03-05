e9fc49583e5dd3d8efc274a473dcdb5b3e0d9825
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-05 13:11:24 +0530

    🎉 My first SWR request

--- app/SWR/basics/page.tsx
+++ app/SWR/basics/page.tsx
@@ -1,8 +1,22 @@
-"use client"
+"use client";
+import { Suspense } from "react";
+import useSWR from "swr";
+
 export default function SWR101() {
-  return (
-    <>
-        <h1>Routing works!</h1>
-    </>
-  );
+  console.log("render");
+  const fetcher = function () {
+    return fetch(`https://swapi.dev/api/people/1`)
+      .then((res) => res.json())
+      .then((data) => data);
+  };
+
+  const { data, error, isLoading } = useSWR(`/test/one`, fetcher);
+
+  console.log(data);
+  const loadingJSX = <h1>Loading</h1>;
+  const errorJSX = <h1>Error</h1>;
+
+  if (error) return errorJSX;
+  if (isLoading) return loadingJSX;
+  return <h1>{data.name}</h1>;
 }
