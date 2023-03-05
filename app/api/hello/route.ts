254318e9797241b9a1df9b2e731d6efd5cbf17e4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-05 14:51:52 +0530

    Fetch inside an API route

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,10 @@
+async function fetchPerson() {
+  const response = await fetch(`https://swapi.dev/api/people/1`);
+  const data = await response.json();
+  return data;
+}
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const person = await fetchPerson();
+  return new Response(`${person.name}`);
 }
