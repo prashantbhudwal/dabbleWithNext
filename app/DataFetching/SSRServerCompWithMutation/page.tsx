9dd673d37b74dee0c732551fb2d849805dc20ec4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-07 15:23:55 +0530

    Add cache: no-store to enable SSR

--- app/DataFetching/SSRServerCompWithMutation/page.tsx
+++ app/DataFetching/SSRServerCompWithMutation/page.tsx
@@ -5,7 +5,9 @@ interface Pokemon {
 }
 
 export default async function SSRServerComp() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12", {
+    cache: "no-store",
+  });
   const data = await response.json();
 
   const pokemonData: Pokemon[] = data.results;
