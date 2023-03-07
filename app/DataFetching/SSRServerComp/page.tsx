8a3f172cc6c1368476a23041161a13d45b140f93
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-07 14:40:56 +0530

    LO: server Comps provide Cookies and Header Fns

--- app/DataFetching/SSRServerComp/page.tsx
+++ app/DataFetching/SSRServerComp/page.tsx
@@ -1,13 +1,20 @@
+import { headers } from "next/headers";
+import { cookies } from "next/headers";
+import PokemonFetcher from "./PokemonFetcher";
 interface Pokemon {
   name: string;
   url: string;
 }
-import PokemonFetcher from "./PokemonFetcher";
+
 export default async function SSRServerComp() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
   const data = await response.json();
 
   const pokemonData: Pokemon[] = data.results;
 
-  return <PokemonFetcher pokemonData={pokemonData}/>;
+  // Next.js provides helpful server functions you may need when fetching data in Server Components:
+  const headerFromPokeAPI = headers();
+  const cookieFromPokeAPI = cookies();
+
+  return <PokemonFetcher pokemonData={pokemonData} />;
 }
