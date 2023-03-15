f75c135037ee5e32f7d957b1fd8468e69432529e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-15 09:58:05 +0530

    LO/R/State: Mock all visual states in a Storybook

--- app/React/State/ReactingToInput/page.tsx
+++ app/React/State/ReactingToInput/page.tsx
@@ -1,26 +1,48 @@
 export function ReactingToInput({ status = "empty" }: { status?: string }) {
-  if (status === "empty") {
-    return <div className="border border-fuchsia-500">Damn Right!!</div>;
+  if (status === "success") {
+    return (
+      <div className="border border-fuchsia-500 p-4">
+        <h1> The Quiz - {status}</h1>Damn Right!!
+      </div>
+    );
   }
 
   return (
-    <div className="border border-fuchsia-500">
-      <h1> The Quiz</h1>
-      <form className="flex flex-col gap-3 items-center">
+    <div className="border border-fuchsia-500 p-4">
+      <h1> The Quiz - {status} state</h1>
+      <form className="flex flex-col gap-2 items-center p-3">
         <p> What do you think about server components?</p>
-        <input type={"textarea"} className="border h-14"></input>
-        <button className="border"> Submit</button>
+        <textarea
+          placeholder="Write good things here..."
+          className="border h-14 disabled:placeholder:text-gray-300"
+          disabled={status == "submitting"}
+        ></textarea>
+        <button
+          className="border bg-green-500 disabled:bg-slate-400 disabled:text-slate-50 px-4 py-2"
+          disabled={status === "submitting" || status === "empty"}
+        >
+          {" "}
+          Submit
+        </button>
+        {status === "error" && (
+          <p className="text-red-600">
+            You are wrong, server components are good! Try again and write good
+            stuff!
+          </p>
+        )}
       </form>
     </div>
   );
 }
 
-export default function Test() {
+export default function Storybook() {
+  let visualStates = ["empty", "typing", "submitting", "success", "error"];
   return (
     <div className="flex flex-col gap-5">
       <h1>State Storybook</h1>
-      <ReactingToInput />
-      <ReactingToInput status="success" />
+      {visualStates.map((state) => (
+        <ReactingToInput status={state} key={state} />
+      ))}
     </div>
   );
 }
