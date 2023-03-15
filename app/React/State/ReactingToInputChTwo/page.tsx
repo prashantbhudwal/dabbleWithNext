449ad694b1b1a7b50bf7b4db647b67a4b29cb73c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-15 12:35:49 +0530

    Add base styles

--- app/React/State/ReactingToInputChTwo/page.tsx
+++ app/React/State/ReactingToInputChTwo/page.tsx
@@ -1,17 +1,22 @@
+"use client";
 export function ReactingToInputChallengeTwo({ status = "empty" }) {
   return (
-    <div className="border border-amber-500 p-4 flex flex-row gap-3">
-      {status}
-      <form>
+    <div className="border border-amber-500 flex flex-col gap-3">
+      <div className="bg-zinc-300 w-48 px-4 py-2 text-zinc-500 ml-auto">
+        State: {status}
+      </div>
+      <form className="flex flex-col gap-1  p-4">
         <label>
           First name: <b>Jane</b>
-          <input />
+          <input className="border" />
         </label>
         <label>
           Last name: <b>Jacobs</b>
-          <input />
+          <input className="border" />
         </label>
-        <button type="submit">Edit Profile</button>
+        <button className="bg-green-600 py-2 px-4 rounded-md" type="submit">
+          Edit Profile
+        </button>
         <p>
           <i>Hello, Jane Jacobs!</i>
         </p>
@@ -24,10 +29,10 @@ export default function Storybook() {
   const componentStates = ["empty", "editing", "saved"];
 
   return (
-    <>
+    <div className="flex flex-col gap-2">
       {componentStates.map((state) => (
         <ReactingToInputChallengeTwo status={state} key={state} />
       ))}
-    </>
+    </div>
   );
 }
