27d23247319bfed7b8780e23209253b9e8b9ee2d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-15 11:03:57 +0530

    Add logic to drive visual states

--- app/React/State/ReactingToInput/page.tsx
+++ app/React/State/ReactingToInput/page.tsx
@@ -1,8 +1,36 @@
-export function ReactingToInput({ status = "empty" }: { status?: string }) {
+"use client";
+
+import { useState } from "react";
+// export  function ReactingToInput({ status = "empty" }: { status?: string }) {
+export default function ReactingToInput() {
+  const [input, setInput] = useState<string>("");
+  const [status, setStatus] = useState<string>();
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState("");
+
+  const handleClick = async function (e: React.MouseEvent<HTMLElement>) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      const message = await submitForm(input);
+      setSuccess(message);
+      setStatus("success");
+    } catch (err: any) {
+      console.log(err);
+      setStatus("typing");
+      setError(err);
+    }
+  };
+
+  const handleTyping = function (e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setInput(e.target.value);
+  };
+
   if (status === "success") {
     return (
       <div className="border border-fuchsia-500 p-4">
-        <h1> The Quiz - {status}</h1>Damn Right!!
+        <h1> The Quiz - {status}</h1>
+        {success}
       </div>
     );
   }
@@ -11,38 +39,51 @@ export function ReactingToInput({ status = "empty" }: { status?: string }) {
     <div className="border border-fuchsia-500 p-4">
       <h1> The Quiz - {status} state</h1>
       <form className="flex flex-col gap-2 items-center p-3">
-        <p> What do you think about server components?</p>
+        <p> Are Server Components Good? (Y/N)</p>
         <textarea
-          placeholder="Write good things here..."
+          value={input}
+          onChange={handleTyping}
+          placeholder="Y is the only answer"
           className="border h-14 disabled:placeholder:text-gray-300"
           disabled={status == "submitting"}
         ></textarea>
         <button
+          onClick={handleClick}
           className="border bg-green-500 disabled:bg-slate-400 disabled:text-slate-50 px-4 py-2"
-          disabled={status === "submitting" || status === "empty"}
+          disabled={status === "submitting" || input.length === 0}
         >
           {" "}
           Submit
         </button>
-        {status === "error" && (
-          <p className="text-red-600">
-            You are wrong, server components are good! Try again and write good
-            stuff!
-          </p>
-        )}
+        {error != null && <p className="text-red-600">{error.message}</p>}
       </form>
     </div>
   );
 }
 
-export default function Storybook() {
-  let visualStates = ["empty", "typing", "submitting", "success", "error"];
-  return (
-    <div className="flex flex-col gap-5">
-      <h1>State Storybook</h1>
-      {visualStates.map((state) => (
-        <ReactingToInput status={state} key={state} />
-      ))}
-    </div>
-  );
+function submitForm(answer: string) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      answer === "Y"
+        ? resolve("Damn Right!")
+        : reject(
+            new Error(
+              "You are wrong, server components are good! Try again and write good stuff!"
+            )
+          );
+    }, 2000);
+  });
+  return promise;
 }
+
+// export default function Storybook() {
+//   let visualStates = ["empty", "typing", "submitting", "success", "error"];
+//   return (
+//     <div className="flex flex-col gap-5">
+//       <h1>State Storybook</h1>
+//       {visualStates.map((state) => (
+//         <ReactingToInput status={state} key={state} />
+//       ))}
+//     </div>
+//   );
+// }
