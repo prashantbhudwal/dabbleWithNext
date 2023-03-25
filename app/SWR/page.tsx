639b2d54092a760c9ab9b4952d79a2675c381046
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-25 14:58:05 +0530

    Add new routes

--- app/SWR/page.tsx
+++ app/SWR/page.tsx
@@ -3,10 +3,12 @@ export default function SWR() {
   return (
     <>
       <h1>Learn SWR</h1>
-      <div className="underline text-violet-500 flex flex-col">
+      <div className="flex flex-col text-violet-500 underline">
         <Link href="/SWR/basics/"> Basics </Link>
         <Link href="/SWR/fetchAPIRoute/"> Fetch from a self hosted API </Link>
         <Link href="/SWR/postToAPIRoute/"> Post to a self hosted API </Link>
+        <Link href="/SWR/mutateWithoutSWR/"> Mutate without SWR </Link>
+        <Link href="/SWR/mutateWithSWR/"> Mutate with SWR mutate function</Link>
       </div>
     </>
   );
