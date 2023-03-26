e5ae57aec23e43cc17e2f13c4c051f00c65400fb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-26 13:10:16 +0530

    LO: Use mutate to optimistically update the cache

---
+++ app/SWR/mutateWithSWR/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import useSWR, { mutate } from "swr";
+import { useState, useEffect } from "react";
+
+const fetchCounter = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from ${url}`);
+  }
+  return response.json();
+};
+
+export default function Counter() {
+  const { data, mutate } = useSWR("/api/counter/", fetchCounter, {
+    refreshInterval: 1000,
+  });
+
+  const handleIncrement = async () => {
+    const newCount = (data?.count || 0) + 1;
+
+    // Optimistically update the cache
+    mutate({ count: newCount }, false);
+
+    // Update the server
+  };
+
+  return (
+    <div>
+      <h1>Counter: {data?.count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+}
