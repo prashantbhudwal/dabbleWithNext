83afbd1d43b8a622dcc833c3c69b4bb2a0e29f22
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-27 19:41:04 +0530

    Add a draggable card component

---
+++ app/DnD/ExampleApp/Card.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useDrag } from "react-dnd";
+import { ItemType } from "./types/itemType";
+
+export const Card = ({ prompt, label }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemType.CARD,
+    item: { prompt },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+      className="m-2 cursor-pointer rounded bg-gray-200 p-3"
+    >
+      {label}
+    </div>
+  );
+};
