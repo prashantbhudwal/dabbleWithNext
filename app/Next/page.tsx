aabecf7103194184a5de9a88d53d3118ad7b93d3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-29 11:59:20 +0530

    Move all conventions to Conventions route

--- app/Next/page.tsx
+++ app/Next/page.tsx
@@ -4,9 +4,9 @@ export default function Next() {
   return (
     <>
       <div className="flex flex-col gap-3 text-blue-500 underline">
+        <Link href="/Next/Conventions">Conventions</Link>
         <Link href="/Next/DataFetching"> Data Fetching </Link>
         <Link href="/Next/Routing">Routing</Link>
-        <Link href="/Next/Layout">Layout</Link>
       </div>
     </>
   );
