41b4bbce337271bdbed0f42360f9361eff420adf
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-29 12:33:46 +0530

    Add an API route that gives a delayed response

--- app/api/delayedResponse/route.ts
+++ app/api/delayedResponse/route.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export async function GET(request: Request) {
+  await delay(2000);
   const response = await fetch(`https://swapi.dev/api/people/2`);
   const data = await response.json();
   const person = await data;
