174fe89c252b0664547e35e0dc1a6f2bb30447e5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-03 08:38:58 +0530

    LO: useRef does not rerender

--- app/React/useRef/chTwo-toggle/page.tsx
+++ app/React/useRef/chTwo-toggle/page.tsx
@@ -1,16 +1,28 @@
-"use client"
-import { useRef } from "react";
+"use client";
+import { useState, useRef } from "react";
+import Button from "@/app/Components/Button";
+import ButtonPanel from "@/app/Components/ButtonPanel";
 
 export default function Toggle() {
+  const [isOn, setIsOn] = useState(false);
   const isOnRef = useRef(false);
 
   return (
-    <button
-      onClick={() => {
-        isOnRef.current = !isOnRef.current;
-      }}
-    >
-      {isOnRef.current ? "On" : "Off"}
-    </button>
+    <ButtonPanel>
+      <Button
+        onClick={() => {
+          setIsOn((isOn) => !isOn);
+        }}
+      >
+        {isOn ? "State: On" : "State: Off"}
+      </Button>
+      <Button
+        onClick={() => {
+          isOnRef.current = !isOnRef.current;
+        }}
+      >
+        {isOnRef.current ? "Ref: On" : "Ref: Off"}
+      </Button>
+    </ButtonPanel>
   );
 }
