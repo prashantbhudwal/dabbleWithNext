fc792c9b5f9be37072dfc144a544af587c26b688
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-04 20:52:07 +0530

    Update fetch url

--- app/Prisma/ReadingData/page.tsx
+++ app/Prisma/ReadingData/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-import { PrismaClient, User } from "@prisma/client";
+import {User } from "@prisma/client";
 import useSWR from "swr";
 
 export default function ReadingDatabase() {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-  const { data: users, error } = useSWR("/api/readWithPrisma", fetcher);
+  const { data: users, error } = useSWR("/api/dabbleWithPrisma", fetcher);
 
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {users && users.map((user: User) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
+        {users && users.map((user: User) => <li key={user.id}>{user.name}</li>)}
       </ul>
     </div>
   );
