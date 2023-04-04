b226a0c33141b2f15a8906044129f9eda5a67949
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-04 20:51:26 +0530

    Add route to read and write data to the database(Prisma)

---
+++ app/api/dabbleWithPrisma/route.tsx
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient, User } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function fetchAllUsers(): Promise<User[]> {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    console.error(error);
+    return [];
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function GET(request: NextRequest) {
+  console.log("GetRan");
+  const fetchedUsers = await fetchAllUsers();
+  console.log(fetchedUsers);
+  return NextResponse.json(fetchedUsers);
+}
+
+async function createUser(userData: User) {
+  try {
+    await prisma.user.create({
+      data: userData,
+    });
+    console.log("User created successfully");
+  } catch (error) {
+    console.error("Error creating user: ", error);
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  await createUser(data);
+  const body = "Users Created";
+  return NextResponse.json({ body });
+}
