f29de6b7aca851676da6e2ebf8851d1685d3e781
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-04 20:36:15 +0530

    Display data fetched from supabase via Prisma

--- app/Prisma/ReadingData/page.tsx
+++ app/Prisma/ReadingData/page.tsx
@@ -1,36 +1,17 @@
 "use client";
-import { useState, useEffect } from "react";
 import { PrismaClient, User } from "@prisma/client";
-const prisma = new PrismaClient();
-
-async function fetchAllUsers(): Promise<User[]> {
-  try {
-    const users = await prisma.user.findMany();
-    return users;
-  } catch (error) {
-    console.error(error);
-    return [];
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+import useSWR from "swr";
 
 export default function ReadingDatabase() {
-  const [users, setUsers] = useState<User[]>([]);
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-  useEffect(() => {
-    async function getUsers() {
-      const fetchedUsers = await fetchAllUsers();
-      setUsers(fetchedUsers);
-    }
+  const { data: users, error } = useSWR("/api/readWithPrisma", fetcher);
 
-    getUsers();
-  }, []);
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {users.map((user) => (
+        {users && users.map((user: User) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
