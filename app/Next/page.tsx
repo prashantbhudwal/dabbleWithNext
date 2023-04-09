89b893b9c40ecfc19f125e46b4d6a3791de04735
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-09 18:06:56 +0530

    Add Readable Stream

--- app/Next/page.tsx
+++ app/Next/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
+import LinkTree from "../Components/LinkTree";
 
 export default function Next() {
   return (
-    <>
-      <div className="flex flex-col gap-3 text-blue-500 underline">
-        <Link href="/Next/Conventions">Conventions</Link>
-        <Link href="/Next/DataFetching"> Data Fetching </Link>
-        <Link href="/Next/Routing">Routing</Link>
-      </div>
-    </>
+    <LinkTree>
+      <Link href="/Next/Conventions">Conventions</Link>
+      <Link href="/Next/DataFetching"> Data Fetching </Link>
+      <Link href="/Next/Routing">Routing</Link>
+      <Link href="/Next/ReadableStreams">Readable Streams</Link>
+    </LinkTree>
   );
 }
