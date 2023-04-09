230662ee447197d381a7f5d57fd6f782e13ae63b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-09 20:38:22 +0530

    Read and decode the stream

--- app/Next/ReadableStreams/page.tsx
+++ app/Next/ReadableStreams/page.tsx
@@ -1,19 +1,40 @@
 "use client";
-import { useEffect } from "react";
 import { useState } from "react";
 import Button from "@/app/Components/Button";
+
 export default function Test() {
   const [stream, setStream] = useState<string>("");
+
   const fetchStream = async function () {
     const response = await fetch(`/api/readableStreams/one`);
-    const data = await response.json();
-    setStream(data);
+    const readableStream = response.body;
+    const reader = readableStream?.getReader();
+    const decoder = new TextDecoder(); // Add this line
+
+    if (reader) {
+      let done = false;
+      while (!done) {
+        try {
+          const { value, done: streamDone } = await reader.read();
+          done = streamDone;
+          if (!done) {
+            const decodedValue = decoder.decode(value);
+            console.log(decodedValue);
+            setStream((prevStream) => prevStream + decodedValue);
+          }
+        } catch (error) {
+          console.error("Error reading stream:", error);
+          done = true;
+        }
+      }
+      console.log("done");
+    }
   };
 
   return (
     <div>
       <Button onClick={fetchStream}>Fetch Stream</Button>
-      {stream}
+      <div>{stream}</div>
     </div>
   );
 }
