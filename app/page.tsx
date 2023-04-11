fdf90845b881cb6658f07ab91ca173a0f64251cb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-11 19:06:49 +0530

    Add route for Jotai

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import LinkTree from "@/app/Components/LinkTree";
 export default function Home() {
   return (
     <LinkTree>
-      <Link href="/React"> Learn React 18</Link>
-      <Link href="/SWR"> Learn SWR </Link>
-      <Link href="/Next"> Learn NextJs 13 </Link>
-      <Link href="/DnD"> Learn React Drag And Drop </Link>
-      <Link href="/Supabase"> Learn Supabase </Link>
-      <Link href="/Prisma"> Learn Prisma </Link>
+      <Link href="/React"> React 18</Link>
+      <Link href="/SWR"> SWR </Link>
+      <Link href="/Next"> NextJs 13 </Link>
+      <Link href="/DnD"> React Drag And Drop </Link>
+      <Link href="/Supabase"> Supabase </Link>
+      <Link href="/Prisma"> Prisma </Link>
+      <Link href="/Jotai"> Jotai </Link>
     </LinkTree>
   );
 }
