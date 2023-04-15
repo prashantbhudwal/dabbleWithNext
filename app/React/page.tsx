7e899ec58b82b9b6fa1bc2c19fbed5981fe2bd86
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-15 22:48:41 +0530

    Remove unused code

--- app/React/page.tsx
+++ app/React/page.tsx
@@ -3,18 +3,10 @@ import LinkTree from "../Components/LinkTree";
 export default function React() {
   return (
     <LinkTree>
-      <div>
-        <Link href="/React/State/"> State </Link>
-      </div>
-      <div>
-        <Link href="/React/SideEffects/"> Side Effects </Link>
-      </div>
-      <div>
-        <Link href="/React/useEffect/"> UseEffect </Link>
-      </div>
-      <div>
-        <Link href="/React/useRef/"> UseRef </Link>
-      </div>
+      <Link href="/React/State/"> State </Link>
+      <Link href="/React/SideEffects/"> Side Effects </Link>
+      <Link href="/React/useEffect/"> UseEffect </Link>
+      <Link href="/React/useRef/"> UseRef </Link>
     </LinkTree>
   );
 }
