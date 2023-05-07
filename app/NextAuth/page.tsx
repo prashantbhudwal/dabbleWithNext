8d8a9fb70b9fe86dd71890cd19328d5ec8df0689
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-08 02:09:45 +0530

    Add SignIn button

--- app/NextAuth/page.tsx
+++ app/NextAuth/page.tsx
@@ -1,11 +1,16 @@
+import Button from "../Components/Button";
 import LinkTree from "../Components/LinkTree";
 import Link from "next/link";
+import SignInBtn from "./SignInBtn";
 
 export default function NextAuth() {
   return (
-    <LinkTree>
-      <Link href="/NextAuth/Protected">NextAuth</Link>
-      <Link href="/NextAuth/Protected">NextAuth</Link>
-    </LinkTree>
+    <div className="flex-col items-center gap-2">
+      <LinkTree>
+        <Link href="/NextAuth/Protected">NextAuth</Link>
+        <Link href="/NextAuth/Protected">NextAuth</Link>
+      </LinkTree>
+      <SignInBtn />
+    </div>
   );
 }
