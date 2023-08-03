61b8c80d3cdde2d9d52fe08f3f911deb9ae9e8e8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-03 23:59:40 +0530

    track event on signing in

--- app/NextAuth/SignInBtn.tsx
+++ app/NextAuth/SignInBtn.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { trackSignOut } from "@/mixpanel/actions";
 import Button from "../Components/Button";
 import { signIn, signOut, useSession } from "next-auth/react";
 export default function SignInBtn() {
   const { data: session } = useSession();
+  const handleSignOut = () => {
+    signOut();
+    trackSignOut(session);
+  };
   return session && session.user ? (
-    <Button onClick={() => signOut()}>Sign Out</Button>
+    <Button onClick={() => handleSignOut()}>Sign Out</Button>
   ) : (
     <Button onClick={() => signIn()}>Sign In</Button>
   );
