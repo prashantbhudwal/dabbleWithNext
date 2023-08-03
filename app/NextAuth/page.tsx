5a36490ab3e49f5577e20dbf56616ac6d3eb895a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-03 23:58:53 +0530

    Use mixpanel in server components

--- app/NextAuth/page.tsx
+++ app/NextAuth/page.tsx
@@ -1,9 +1,14 @@
+import { mp } from "@/mixpanel";
 import Button from "../Components/Button";
 import LinkTree from "../Components/LinkTree";
 import Link from "next/link";
 import SignInBtn from "./SignInBtn";
 
 export default function NextAuth() {
+  mp.track("PageView",{
+    "Page Name": "NextAuth",
+    "Page Path": "/NextAuth",
+  });
   return (
     <div className="flex-col items-center gap-2">
       <LinkTree>
