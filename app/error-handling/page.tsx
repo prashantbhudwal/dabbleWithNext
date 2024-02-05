59972e9808da9b00f7bc569ef2cb861550debec6
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-05 15:52:13 +0530

    Update error handling links

--- app/error-handling/page.tsx
+++ app/error-handling/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 function ErrorHandling() {
   return (
     <div className="flex flex-col gap-2 text-teal-700 underline">
-      <Link href="/Next/error-handling/vanilla/"> Vanilla Error Handling </Link>
-      <Link href="/Next/error-handling/withFP/"> With fp-ts </Link>
+      <Link href="/error-handling/vanilla/"> Vanilla Error Handling </Link>
+      <Link href="/error-handling/withFp/"> With fp-ts </Link>
     </div>
   );
 }
