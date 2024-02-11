75864510a1be47043d9ea777df61599d01057abe
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-11 08:11:43 +0530

    Refactor LinkTree component to support nested LinkTrees

--- app/Components/LinkTree.tsx
+++ app/Components/LinkTree.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { Children, ReactElement, ReactNode, cloneElement } from "react";
 
 interface LinkListProps {
   children: ReactNode[];
@@ -15,6 +15,52 @@ export default function LinkTree({ children }: LinkListProps) {
           {child}
         </li>
       ))}
+    </ul>
+  );
+}
+
+interface LinkListProps {
+  children: ReactNode[];
+  depth?: number;
+}
+
+// The LinkTree component now can take any ReactNode, including other LinkTree components.
+export function LinkTreeNested({
+  children,
+  depth = 0,
+}: LinkListProps): ReactElement {
+  return (
+    <ul className={`flex flex-col ${depth > 0 ? "pl-4" : ""} w-full`}>
+      {Children.map(children, (child, index) => {
+        const isLinkTree =
+          React.isValidElement(child) &&
+          "children" in (child as ReactElement).props;
+
+        if (isLinkTree) {
+          // Increment depth for nested LinkTrees
+          const newDepth = depth + 1;
+          // Clone the element to inject the depth prop, enabling styling based on nesting level
+          const childWithProps = cloneElement(child as ReactElement, {
+            depth: newDepth,
+          });
+
+          return (
+            <li key={index} className="m-1">
+              {childWithProps}
+            </li>
+          );
+        }
+
+        // Apply different styles based on depth
+        return (
+          <li
+            key={index}
+            className={`m-1 inline-flex rounded-full ${depth === 0 ? "bg-violet-600" : "bg-violet-400"} px-4 py-3 text-slate-100`}
+          >
+            {child}
+          </li>
+        );
+      })}
     </ul>
   );
 }
