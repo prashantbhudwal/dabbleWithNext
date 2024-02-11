a8d7eda37ed6fa4a0d9440b75b1e4272f34af6b6
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-11 08:00:50 +0530

    Refactor page.tsx to include LinkTree component

--- app/Typescript/page.tsx
+++ app/Typescript/page.tsx
@@ -1,7 +1,11 @@
-export default async function Test() {
+import Link from "next/link";
+import LinkTree from "@/app/Components/LinkTree";
+
+export default function TS() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <LinkTree>
+      <Link href="/typescript/one/"> One </Link>
+      <Link href="/typescript/generics/"> One </Link>
+    </LinkTree>
   );
 }
