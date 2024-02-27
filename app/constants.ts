8cf9ed6afa947b533585128a6db8b22e45bc23a4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-27 06:29:46 +0530

    Add constants for long text

---
+++ app/constants.ts
@@ -0,0 +1,15 @@
+const fortyWords = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem tenetur
+        soluta sunt dolorum rerum vel reiciendis perferendis quibusdam illum
+        assumenda quod suscipit ducimus reprehenderit explicabo, porro ut eum
+        corrupti iure excepturi ipsam tempora quidem quisquam voluptates ipsa?
+        Aliquam, autem eos.`;
+
+const sixtyWords = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo asperiores ullam totam nostrum, doloremque ea sunt animi doloribus? Ad quidem impedit iusto quae nemo, quos officia atque, perspiciatis obcaecati cupiditate fuga voluptatum aperiam rerum minus totam voluptate laudantium blanditiis illo repellat error. Facere vitae magni, excepturi iusto ea blanditiis modi itaque voluptates alias nesciunt temporibus quas ipsam non impedit sunt.`;
+
+const hundredWords = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis porro, magni eum aliquid unde quam voluptates architecto exercitationem consectetur culpa nisi. Accusantium tempora numquam blanditiis expedita quis fugit necessitatibus labore ratione voluptatibus molestias consequuntur nihil ducimus provident, excepturi beatae deleniti quaerat laudantium aliquam perspiciatis magnam alias quos at! Dignissimos laudantium corrupti ullam pariatur illum dolorum quam quisquam, vel, rerum molestias non cum est quis doloribus dolores ipsa architecto, delectus eligendi officia sequi odit? Expedita, repellendus. Repudiandae adipisci magnam sequi temporibus harum reiciendis consectetur voluptas repellat! Ab possimus fugit repellendus mollitia cupiditate rerum eos id ullam, blanditiis neque nulla soluta magnam.`;
+
+export const longText = {
+  fortyWords,
+  sixtyWords,
+  hundredWords,
+};
