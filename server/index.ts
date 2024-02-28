5a5c7431d88fd32a95925b6e802f36c2b4710984
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-28 08:19:56 +0530

    Add basic HTML page and CSS styles

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 const port = 3001;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World with Express and TypeS!");
+  // Resolve and normalize the file path
+  const filePath = path.resolve(
+    path.normalize(path.join(__dirname, "./basic-page/index.html"))
+  );
+
+  // Send the HTML file
+  res.sendFile(filePath);
 });
 
 app.listen(port, () => {
